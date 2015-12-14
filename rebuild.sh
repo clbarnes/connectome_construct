e0982fd283daa393cfd91c8f23fac393c840eb34
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2015-12-14 17:06:19 -0500

    add ser-2 in VD, remove Buyun's data

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+source activate construct
 echo Constructing physical network...
 python3 -m physical
 echo Constructing extrasynaptic network...
@@ -7,4 +9,7 @@ echo Constructing metadata...
 python3 -m metadata
 echo Combining...
 python3 -m combine
-echo Finished! Output in ./combine/tgt_data
+source deactivate
+echo Finished! Output in ./combine/tgt_data 
+echo "Don't forget to commit the changes"
+

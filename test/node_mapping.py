3f666b4b6155f51356fd9ca116618d2702c4b51a
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-05-12 18:11:33 -0400

    make csv of node mappings for mid zero purposes

---
+++ test/node_mapping.py
@@ -0,0 +1,32 @@
+import csv
+import os
+import re
+
+zero_re = re.compile('(?<=[A-Z])0(?=[1-9])')
+
+
+def ensure_mid_zero(s):
+    return zero_re.sub('', s)
+
+
+barry_root = '/home/cbarnes/work/code/connectome/construct2/test/barry_data'
+
+
+def get_node_tuples():
+    nodeset = set()
+    for path in os.listdir(barry_root):
+        with open(os.path.join(barry_root, path)) as f:
+            reader = csv.reader(f, delimiter=' ')
+            for src, tgt, weight in reader:
+                nodeset.update([src, tgt])
+
+    lst = sorted((node, ensure_mid_zero(node)) for node in nodeset)
+    assert len(lst) == 302
+    return lst
+
+with open('node_mapping.csv', 'w') as f:
+    writer = csv.writer(f)
+    for tup in get_node_tuples():
+        writer.writerow(tup)
+
+
